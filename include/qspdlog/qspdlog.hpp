6c75e5ff56e7d4617cfdc34f73cbb4d00a5eda7d
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-12-24 17:15:25 +0400

    Add essential documentation and comments

--- include/qspdlog/qspdlog.hpp
+++ include/qspdlog/qspdlog.hpp
@@ -17,12 +17,48 @@ class QSpdLogProxyModel;
 class QSpdLog : public QTreeView
 {
 public:
+    /**
+     * @brief Constructor
+     *
+     * @param parent The parent widget.
+     */
     explicit QSpdLog(QWidget* parent = nullptr);
+
+    /**
+     * @brief Destructor
+     */
     ~QSpdLog();
 
 public:
+    /**
+     * @brief Clear the contents of the model.
+     *
+     * The method will clear up all the cached messages. There's no way after
+     * this to restore them.
+     */
     void clear();
+
+    /**
+     * @brief Get the toolbar widget.
+     *
+     * The toolbar widget is constructed by the QSpdLog class and has all the
+     * required connections already set up.
+     *
+     * The toolbar provides the controls for setting up the auto-scroll policy
+     * and tools for filtering the messages.
+     *
+     * @return the toolbar widget
+     */
     QWidget* toolbar() const;
+
+    /**
+     * @brief Get the sink.
+     *
+     * The sink should be used by the user to add it into any logger whose
+     * output the user want's to see in the widget.
+     *
+     * @return std::shared_ptr<spdlog::sinks::sink> the sink of the widget
+     */
     std::shared_ptr<spdlog::sinks::sink> sink();
 
 private slots:
