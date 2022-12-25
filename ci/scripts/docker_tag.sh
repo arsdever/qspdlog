f0cc260caa50b6fdfa8dd8b2132c9d7ecaa76219
Arsen <arsen.gharagyozyn.96@gmail.com>
2022-12-25 17:24:12 +0400

    Improve workflows for building dockerimages (#22)
    * Improve workflows for building dockerimages
    
    - Split linux build job
      1. separate build docker image
      2. separate build linux app
      3. separate delete docker image
    - Add scripts for manipulating docker data
      1. get tag from the PR data
      2. script for build/push/delete docker image
    
    * Add +x permission for ci/scripts scripts
    
    * Fix set-output command
    
    * Change to modern version of setting output
    
    * Fix script name in the workflow file
    
    * Use GITHUB_REF_NAME instead of GITHUB_REF
    
    * Extract branch name from the GITHUB_REF manually
    
    * Revert last change and add echo
    
    * Use GITHUB_HEAD_REF instead of GITHUB_REF_NAME
    
    * Remove echoing of the branch name
    
    * Update names of the workflows
    
    * Reorder for getting paths-filter output
    
    * Update linux job to use tag provided by docker build workflow

---
+++ ci/scripts/docker_tag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Enable exit on error
+set -e
+
+# Check if the docker image is being built on main brach
+# and set the correct outputs for the step
+if [ "$GITHUB_EVENT_NAME" == "push" ] && \
+	[ "$GITHUB_REF_NAME" == "main" ]; then
+	echo "tag=latest" > $GITHUB_OUTPUT;
+else
+	echo "tag=pr-$GITHUB_HEAD_REF" > $GITHUB_OUTPUT;
+fi
