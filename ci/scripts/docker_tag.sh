cdf1112333f22eb56c71fe9465f44d6dcecd0ec5
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-12-25 16:52:41 +0400

    Revert last change and add echo

--- ci/scripts/docker_tag.sh
+++ ci/scripts/docker_tag.sh
@@ -3,14 +3,13 @@
 # Enable exit on error
 set -e
 
-# Extract branch name from the ref
-branch="${GITHUB_REF#refs/heads/}"
+echo "The branch name is $GITHUB_REF_NAME"
 
 # Check if the docker image is being built on main brach
 # and set the correct outputs for the step
 if [ "$GITHUB_EVENT_NAME" == "push" ] && \
-	[ $branch == "main" ]; then
+	[ "$GITHUB_REF_NAME" == "main" ]; then
 	echo "tag=latest" > $GITHUB_OUTPUT;
 else
-	echo "tag=pr-$branch" > $GITHUB_OUTPUT;
+	echo "tag=pr-$GITHUB_REF_NAME" > $GITHUB_OUTPUT;
 fi
