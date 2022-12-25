0af75212103dace1ea042f910b86a4dd6e286c80
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-12-25 16:41:26 +0400

    Change to modern version of setting output

--- ci/scripts/docker_tag.sh
+++ ci/scripts/docker_tag.sh
@@ -6,8 +6,8 @@ set -e
 # Check if the docker image is being built on main brach
 # and set the correct outputs for the step
 if [ "$GITHUB_EVENT_NAME" == "push" ] && \
-	[ "$GITHUB_REF" == "refs/heads/main" ]; then \
-	echo "::set-output name=tag::latest";
+	[ "$GITHUB_REF" == "refs/heads/main" ]; then
+	echo "tag=latest" > $GITHUB_OUTPUT;
 else
-	echo "::set-output name=tag::pr-$GITHUB_REF"; \
+	echo "tag=pr-$GITHUB_REF" > $GITHUB_OUTPUT;
 fi
