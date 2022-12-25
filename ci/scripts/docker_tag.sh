d8b588d9634fff98e895f5f27bfb9e1d3fb190a2
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-12-25 16:30:31 +0400

    Improve workflows for building dockerimages
    - Split linux build job
      1. separate build docker image
      2. separate build linux app
      3. separate delete docker image
    - Add scripts for manipulating docker data
      1. get tag from the PR data
      2. script for build/push/delete docker image

---
+++ ci/scripts/docker_tag.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Enable exit on error
+set -e
+
+# Check if the docker image is being built on main brach
+# and set the correct outputs for the step
+if [ "$GITHUB_EVENT_NAME" == "push" ] && \
+	[ "$GITHUB_REF" == "refs/heads/main" ]; then \
+	echo "::set-output tag=::latest";
+else
+	echo "::set-output tag=::pr-$GITHUB_REF"; \
+fi
