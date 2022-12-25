4338bcff71c628e6b61d5ea5ffd5bd42d57a330f
Arsen Gharagyozyan <arsen.gharagyozyn.96@gmail.com>
2022-12-25 16:58:05 +0400

    Use GITHUB_HEAD_REF instead of GITHUB_REF_NAME

--- ci/scripts/docker_tag.sh
+++ ci/scripts/docker_tag.sh
@@ -3,7 +3,7 @@
 # Enable exit on error
 set -e
 
-echo "The branch name is $GITHUB_REF_NAME"
+echo "The branch name is $GITHUB_HEAD_REF"
 
 # Check if the docker image is being built on main brach
 # and set the correct outputs for the step
@@ -11,5 +11,5 @@ if [ "$GITHUB_EVENT_NAME" == "push" ] && \
 	[ "$GITHUB_REF_NAME" == "main" ]; then
 	echo "tag=latest" > $GITHUB_OUTPUT;
 else
-	echo "tag=pr-$GITHUB_REF_NAME" > $GITHUB_OUTPUT;
+	echo "tag=pr-$GITHUB_HEAD_REF" > $GITHUB_OUTPUT;
 fi
